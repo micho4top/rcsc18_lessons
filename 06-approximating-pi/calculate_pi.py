7e5766bb8eb9f467ef368590f6698420ca14d9f5
micho4top <cmm41@le.ac.uk>
2018-09-06 09:15:24 +0100

    The recieved

---
+++ 06-approximating-pi/calculate_pi.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy import random
+
+def calculate_pi(n):
+	inside = 0
+	outside = 0
+
+	for i in range(n)
+		x = random.random()
+		y = random.random()
+
+		r = np.sqrt((x**2) + (y**2))
+
